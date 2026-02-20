b50af55c08ad7b8f8047996bb7cb578ba05a226e
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-02-19 19:42:15 -0700

    Populate kanji page with data from kanji.json

---
+++ kanji.js
@@ -0,0 +1,51 @@
+const body = document.querySelector("body");
+
+async function populate() {
+    const file = "./kanji.json";
+    const request = new Request(file);
+
+    const response = await fetch(request);
+    const kanjiList = await response.json();
+
+    const kanjiChar = document.createElement("p");
+    const kun = document.createElement("p");
+    const on = document.createElement("p");
+    const kunVocab = document.createElement("p");
+    const onVocab = document.createElement("p");
+
+
+    body.appendChild(kanjiChar);
+    body.appendChild(kun);
+    body.appendChild(on);
+    body.appendChild(kunVocab);
+    body.appendChild(onVocab);
+
+    kun.textContent = kanjiList[0].kun;
+    on.textContent = kanjiList[0].on;
+    kanjiChar.textContent = kanjiList[0].kanji;
+
+    addRuby(kunVocab, kanjiList[0].kunVocab, kanjiList[0].kunVocabRuby);
+    addRuby(onVocab, kanjiList[0].onVocab, kanjiList[0].onVocabRuby);
+}
+
+function addRuby(parent, mainText, rubyText) {
+    const ruby = document.createElement("ruby");
+    const rp1 = document.createElement("rp");
+    const rt = document.createElement("rt");
+    const rp2 = document.createElement("rp");
+
+    parent.appendChild(ruby);
+    ruby.textContent = mainText;
+
+    ruby.appendChild(rp1);
+    ruby.appendChild(rt);
+    ruby.appendChild(rp2);
+
+    rp1.textContent = "("
+    rt.textContent = rubyText;
+    rp2.textContent = ")";
+
+    console.log(rp1.textContent);
+}
+
+populate();
