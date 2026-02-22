c8c83ecd2f31b704259784d7d3ffdefa32e6d034
Brody Livingston <57334476+brody728@users.noreply.github.com>
2026-02-22 11:56:03 -0700

    Add english meanings of vocab words to webpage

--- kanji.js
+++ kanji.js
@@ -41,24 +41,30 @@ function addKanjiData(kanji) {
     kanjiChar.textContent = kanji.kanji;
     meaning.textContent = `Meaning: ${kanji.meaning}`;
     kun.textContent = `Kun-yomi: ${kanji.kun}`;
-    kunVocab.textContent = "Kun-yomi Vocabulary: "
+    kunVocab.textContent = "Kun-yomi Vocabulary"
     on.textContent = `On-yomi: ${kanji.on}`;
-    onVocab.textContent = "On-yomi Vocabulary: "
+    onVocab.textContent = "On-yomi Vocabulary"
 
     for (let i = 0; i < kanji.kunVocab.length; i++) {
+        kunVocab.append(document.createElement("br"));
+
         for (let j = 0; j < kanji.kunVocab[i].length; j++) {
             if (kanji.kunVocabRuby[i][j] !== "") addRuby(kunVocab, kanji.kunVocab[i][j], kanji.kunVocabRuby[i][j]);
             else addWithoutRuby(kunVocab, kanji.kunVocab[i][j]);
         }
-        if (i !== kanji.kunVocab.length - 1) kunVocab.append("、");
+
+        kunVocab.append(` ${kanji.kunVocabMeaning[i]}`);
     }
 
     for (let i = 0; i < kanji.onVocab.length; i++) {
+        onVocab.append(document.createElement("br"));
+
         for (let j = 0; j < kanji.onVocab[i].length; j++) {
             if (kanji.onVocabRuby[i][j] !== "") addRuby(onVocab, kanji.onVocab[i][j], kanji.onVocabRuby[i][j]);
             else addWithoutRuby(onVocab, kanji.onVocab[i][j]);
         }
-        if (i !== kanji.onVocab.length - 1) onVocab.append("、");
+
+        onVocab.append(` ${kanji.onVocabMeaning[i]}`);
     }
 }
 
